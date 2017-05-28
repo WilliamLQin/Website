006a29fece3c48a3cf5c97cbd71d92e0cf5ecf97
William <william.qin51@gmail.com>
2017-05-28 12:48:43 -0400

    New Website

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+  $userAgent = $_SERVER['HTTP_USER_AGENT'];
+  $iphone = strpos($userAgent, "iPhone");
+  $android = strpos($userAgent, "Android");
+  $palmpre = strpos($userAgent,"webOS");
+  $berry = strpos($userAgent,"BlackBerry");
+  $ipod = strpos($userAgent,"iPod");
+
+  if ($iphone || $android || $palmpre || $ipod || $berry == true)
+  {
+    require('home.php');
+  }
+  else
+  {
+    require('php/splash.php');
+  }
+  // else
+  // {
+  //   require('home.php');
+  // }
+?>
---
+++ php/splash.php
@@ -0,0 +1,147 @@
+<head>
+  <title>William Qin</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="/css/modules.css">
+  <link rel="icon" type="image/png" href="/resources/Images/favicon.png">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<div class="index">
+  <div class="col-3 height-5 stack hover-all">
+    <div class="overlay">
+      <a href="portfolio.php" class="animated expand-container hidden-container button button-transparent-alt button-border-white">
+        <h3 class="overlay-header">
+          Portfolio
+        </h3>
+      </a>
+    </div>
+    <img src="/resources/Splash/portfolio-3by5.jpeg" alt="portfolio" class="animated expand-image rounded">
+  </div>
+  <div class="col-4 height-2 hover-all stack">
+    <div class="overlay">
+      <a href="achievements.php" class="animated expand-container hidden-container button button-transparent button-border-white">
+        <h3 class="overlay-header">
+          Achievements
+        </h3>
+      </a>
+    </div>
+    <img src="/resources/Splash/achievements-4by2.jpg" alt="achievements" class="animated expand-image rounded">
+  </div>
+
+  <div class="col-2 height-2 stack hover-all">
+    <div class="overlay">
+      <a href="resume.php" class="animated expand-container hidden-container button button-transparent-alt button-border-white">
+        <h3 class="overlay-header">
+          Resume
+        </h3>
+      </a>
+    </div>
+    <img src="/resources/Splash/resume-1by1.jpg" alt="resume" class="animated expand-image rounded">
+  </div>
+
+  <div class="col-3 height-4 right hover-all stack">
+    <div class="overlay">
+      <a href="blog.php" class="animated expand-container hidden-container button button-transparent-alt button-border-white">
+        <h3 class="overlay-header">
+          Blog
+        </h3>
+      </a>
+    </div>
+    <img src="/resources/Splash/blog-3by4.jpeg" alt="blog" class="animated expand-image rounded">
+  </div>
+
+  <div class="col-6 height-3 stack hover-all">
+    <div class="overlay">
+      <a href="about.php" class="animated expand-container hidden-container button button-white button-border-white">
+        <h1 class="title-name">
+          About
+        </h1>
+        <hr noshade width="90%" style="margin-top: 0; margin-bottom: 0;">
+        <h2 class="title-label">
+          Learn more about me!
+        </h2>
+      </a>
+    </div>
+    <a href="about.php" class="animated expand-container hide-container button button-transparent button-border-white">
+      <h1 class="title-name">
+        William Qin
+      </h1>
+      <hr noshade width="90%" style="margin-top: 0; margin-bottom: 0;">
+      <h2 class="title-label">
+        Student & Developer
+      </h2>
+    </a>
+  </div>
+
+  <div class="col-3 height-3 right hover-all stack">
+    <div class="overlay">
+      <a href="contact.php" class="animated expand-container hidden-container button button-transparent-alt button-border-white">
+        <h3 class="overlay-header">
+          Contact
+        </h3>
+      </a>
+    </div>
+    <img src="/resources/Splash/contact-1by1.jpg" alt="contact" class="animated expand-image rounded">
+  </div>
+
+  <div class="col-2 height-2 stack hover-all">
+    <div class="overlay">
+      <a href="https://warplabs.wixsite.com/warplabs" class="animated expand-container hidden-container button button-transparent-alt button-border-white">
+        <p class="overlay-text">
+          Check out Warp Labs!
+        </p>
+      </a>
+    </div>
+    <img src="/resources/Splash/warplabslogo.png" alt="warplabs" class="animated expand-image rounded">
+  </div>
+
+  <div class="col-2 height-2 stack hover-all">
+    <div class="overlay">
+      <a href="" class="animated expand-container hidden-container button button-transparent-alt button-border-white">
+        <h3 class="overlay-header">
+          Featured!
+          <p class="overlay-text">
+            <br>
+            <br>
+            Nothing here yet!
+          </p>
+        </h3>
+      </a>
+    </div>
+    <img src="/resources/Splash/featured-1by1.jpg" alt="featured" class="animated expand-image rounded">
+  </div>
+
+  <div class="col-2 height-2 stack hover-all">
+    <div class="overlay">
+      <a href="archive.php" class="animated expand-container hidden-container button button-transparent-alt button-border-white">
+        <h3 class="overlay-header">
+          Archive
+        </h3>
+      </a>
+    </div>
+    <img src="/resources/Splash/archive-1by1.jpeg" alt="archive" class="animated expand-image rounded">
+  </div>
+
+  <div class="col-1 height-1 hover-all">
+    <a href="https://www.facebook.com/william.qin51">
+      <img src="/resources/Splash/facebooklogo.png" alt="facebook" class="animated expand-image-logoalt">
+    </a>
+  </div>
+  <div class="col-1 height-1 hover-all">
+    <a href="https://github.com/WilliamLQin">
+      <img src="/resources/Splash/githublogo.png" alt="github" class="animated expand-image-logoalt">
+    </a>
+  </div>
+  <div class="col-1 height-1 hover-all">
+    <a href="https://www.linkedin.com/in/williamqin">
+      <img src="/resources/Splash/linkedinlogoTM.png" alt="linkedin" class="animated expand-image">
+    </a>
+  </div>
+
+  <div class="col-3 height-1 hover-all">
+    <a class="animated expand-container button button-grey button-border-white">
+      <h3 class="copyright">
+        &copy William Qin 2016
+      </h3>
+    </a>
+  </div>
+</div>
