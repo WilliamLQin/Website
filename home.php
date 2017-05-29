53b52759e8efbcd3cd01cb309646fe95f9d3c8f5
William <william.qin51@gmail.com>
2017-05-29 19:52:01 -0400

    Header Testing

--- home.php
+++ home.php
@@ -1,3 +1,15 @@
-asdf
-polz
-lmao
+<head>
+  <title>Home | William Qin</title>
+  <?php include('php/imports.php') ?>
+</head>
+<body>
+
+  <?php include('php/header.php') ?>
+
+  <h1>
+    ayy lmao
+  </h1>
+
+  <?php include('php/footer.php') ?>
+
+</body>
---
+++ php/header.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#headerNav">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/home.php/">William Qin</a>
+    </div>
+    <div class="collapse navbar-collapse" id="headerNav">
+      <ul class="nav navbar-nav">
+        <li><a href="#">About</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Resume</a></li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Blog</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
