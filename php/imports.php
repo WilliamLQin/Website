6400e5c7dddd192971d6f9f04225c2a398a86415
William <william.qin51@gmail.com>
2017-05-29 20:01:35 -0400

    Header Testing 3

--- php/imports.php
+++ php/imports.php
@@ -3,6 +3,7 @@
 <meta charset="utf-8">
 <link rel="icon" type="image/png" href="/resources/Images/favicon.png">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="/css/style.css" type="text/css">
 
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
