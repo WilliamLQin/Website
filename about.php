7ea43d0ddb55e4cf1152ae54e09125cb60fd7e5f
William Qin <william.qin51@gmail.com>
2017-06-23 17:44:14 -0400

    carousel

--- about.php
+++ about.php
@@ -34,14 +34,45 @@
           </h3>
         </div>
         <div class="col-sm-6">
-          <?php
-          $image_count = 4;
-          $image_random = rand(1, $image_count);
 
-          echo "hi"
-          //echo "<img src='/resources/Images/william$image_random.JPG>";
-          ?>
-          <img src="/resources/Images/william1.JPG">
+          <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+              <li data-target="#myCarousel" data-slide-to="1"></li>
+              <li data-target="#myCarousel" data-slide-to="2"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner">
+              <div class="item active">
+                <img src="/resources/Images/william1.JPG" alt="Swiss Alps">
+              </div>
+
+              <div class="item">
+                <img src="/resources/Images/william2.JPG" alt="Mount St. Louis Peak">
+              </div>
+
+              <div class="item">
+                <img src="/resources/Images/william3.JPG" alt="Bicentennial Capitol Mall State Park">
+              </div>
+
+              <div class="item">
+                <img src="/resources/Images/william4.JPG" alt="Point Pelee">
+              </div>
+            </div>
+
+            <!-- Left and right controls -->
+            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+              <span class="glyphicon glyphicon-chevron-left"></span>
+              <span class="sr-only">Previous</span>
+            </a>
+            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+              <span class="glyphicon glyphicon-chevron-right"></span>
+              <span class="sr-only">Next</span>
+            </a>
+          </div>
+
         </div>
       </div>
     </div>
