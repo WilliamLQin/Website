0740039ec431f9a7fd100df7de4237c5aa539307
William Qin <william.qin51@gmail.com>
2017-06-23 16:11:50 -0400

    no more text files

--- about.php
+++ about.php
@@ -21,16 +21,15 @@
 
     <div class="jumbotron">
       <div class="container text-spaced">
-        <h1>
-          <?php
-            echo file_get_contents('resources/Text/about-header.txt', FILE_USE_INCLUDE_PATH)
-          ?>
-        </h1>
+        <h2>
+          Hi, I'm William Qin.
+        </h2>
         <h3>
-          <?php
-            echo file_get_contents('resources/Text/about-body.txt', FILE_USE_INCLUDE_PATH);
-          ?>
-          asdf
+          I’m a high school student, programmer, and efficiency enthusiast!
+          <br>
+          With 2 independently published games under my belt, I am actively experimenting and learning new skills to develop interesting games, prototype hardware, and create websites.
+          <br>
+          Outside from active development, I like to play music, do martial arts, snowboard in the winter, and absorb all kinds of art.
         </h3>
       </div>
     </div>
