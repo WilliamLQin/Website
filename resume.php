469e4ede38b53235da4a712a0e2f6d9b870e6268
William Qin <william.qin51@gmail.com>
2017-06-23 18:04:53 -0400

    resume

--- resume.php
+++ resume.php
@@ -20,15 +20,86 @@
     </div>
 
     <div class="jumbotron">
-      <div class="container text-center text-spaced">
+      <div class="container text-spaced">
         <h2>
           Employment Goal
         </h2>
         <hr noshade>
+        <h3>
+          To apply my abilities as a dedicated and proactive high school student in a part-time or temporary full-time entry level position within the information communication technology industry.
+        </h3>
+
         <h2>
           Skills and Qualifications
         </h2>
         <hr noshade>
+        <h3>
+          • Knowledge of C# (Unity), Python, Web Development (HTML, CSS, JavaScript, Bootstrap, jQuery), and Java (Eclipse)
+          <br>
+          • Essential proficiency in Google Drive and Microsoft Office
+          <br>
+          • Understands and works within the dynamics of a group
+          <br>
+          • Manages and resolves conflict when appropriate
+          <br>
+          • Shows interest, initiative, and effort
+          <br>
+          • Willing to continuously learn and grow
+          <br>
+          • Multilingual: Fluent in English, French, Spanish, and Mandarin
+          <br>
+          • Lifesaving Society - Bronze Medallion, Bronze Cross, Emergency First Aid CPR-B & CPR-C
+        </h3>
+
+        <h2>
+          Experience
+        </h2>
+        <hr noshade>
+        <h3>
+          Senior Programmer <br>
+          FRC Team 1325 - Mississauga, Ontario <br>
+          November 16, 2015 – present <br>
+          <br>
+          • Collaborated with team members to program essential robot controls <br>
+          • Took lead for team scouting web app initiative <br>
+          <br>
+          Conservation Youth Corps <br>
+                    Credit Valley Conservation - Mississauga, Ontario <br>
+          August 8 – August 12, 2016 <br>
+          <br>
+          • Conducted environmental stewardship in the Credit Valley watershed <br>
+          • Cooperated with team members to complete tasks – Outstanding Crew Award <br>
+          <br>
+          Legislative Page <br>
+                    Legislative Assembly of Ontario - Toronto, Ontario <br>
+          April 28 – May 6, 2014 & July 7 – 24, 2014 <br>
+          <br>
+          • Acted as a messenger in the Provincial Parliament between politicians and officials <br>
+          • Collaborated with co-workers to effectively complete tasks in a timely manner <br>
+          • Analyzed schedules, rules, and maps to do required job efficiently <br>
+        </h3>
+
+        <h2>
+          Education
+        </h2>
+        <hr noshade>
+        <h3>
+          International Business Technology Program <br>
+          Gordon Graydon Memorial Secondary School - Mississauga, Ontario <br>
+          <br>
+          September 2015 - present <br>
+          &nbsp; &nbsp; Cumulative GPA: 4.0/4.0
+        </h3>
+
+        <h2>
+          Personal Achievements
+        </h2>
+        <hr noshade>
+        <h3>
+          • Winner of Best Overall Hack w/ Mentor at Cipher Local Hack Day 2016 <br>
+          • 1st Place Winner in Business Services Operations Research at DECA Ontario Provincial Competition <br>
+          • Certificate of Outstanding Achievement (Perfect Score) in Waterloo Gauss Math Contest <br>
+        </h3>
       </div>
     </div>
 
