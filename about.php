a0e2acc86a6ecd4a6e6e702d875136ffede2dee3
William Qin <william.qin51@gmail.com>
2017-06-23 17:23:44 -0400

    image to about

--- about.php
+++ about.php
@@ -21,16 +21,27 @@
 
     <div class="jumbotron">
       <div class="container text-spaced">
-        <h2 class="bold">
-          Hi, I'm William Qin.
-        </h2>
-        <h3>
-          I’m a high school student, programmer, and efficiency enthusiast!
-          <br>
-          With 2 independently published games under my belt, I am actively experimenting and learning new skills to develop interesting games, prototype hardware, and create websites.
-          <br>
-          Outside from active development, I like to play music, do martial arts, snowboard in the winter, and absorb all kinds of art.
-        </h3>
+        <div class="col-sm-8">
+          <h2 class="bold">
+            Hi, I'm William Qin.
+          </h2>
+          <h3>
+            I’m a high school student, programmer, and efficiency enthusiast!
+            <br>
+            With 2 independently published games under my belt, I am actively experimenting and learning new skills to develop interesting games, prototype hardware, and create websites.
+            <br>
+            Outside from active development, I like to play music, do martial arts, snowboard in the winter, and absorb all kinds of art.
+          </h3>
+        </div>
+        <div class="col-sm-4">
+          <?php
+          $image_count = 4;
+          $image_random = rand(1, $image_count)
+
+          echo "<img src='/resources/Images/william$image_random.JPG"
+          ?>
+          <!-- <img src="/resources/Images/william1.JPG"> -->
+        </div>
       </div>
     </div>
 
