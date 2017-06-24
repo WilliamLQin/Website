463c687c6e8b707412cca2672c09533aaf3c16bc
William Qin <william.qin51@gmail.com>
2017-06-24 19:27:49 -0400

    add cprvr

--- portfolio.php
+++ portfolio.php
@@ -25,19 +25,20 @@
         <h2 class="bold">
           Here, you'll find a gallery of my projects!
         </h2>
+        <br>
 
         <div class="module-container module-1by1">
           <div class="module">
             <div class="overlay">
-              <a href="#/" id="changer" name="laserfield" class="animated expand-container hidden-container button transparent">
+              <a href="#/" id="changer" name="cprvr" class="animated expand-container hidden-container button transparent">
                 <div class="overlay-header">
                   <h3>
-                    Laser Field
+                    CPR-VR
                   </h3>
                 </div>
               </a>
             </div>
-            <img src="/resources/Images/laserfield1.png" class="animated expand-image rounded border-black">
+            <img src="/resources/Images/CPRVR1.jpg" class="animated expand-image rounded border-black">
           </div>
         </div>
 
@@ -56,6 +57,21 @@
           </div>
         </div>
 
+        <div class="module-container module-1by1">
+          <div class="module">
+            <div class="overlay">
+              <a href="#/" id="changer" name="laserfield" class="animated expand-container hidden-container button transparent">
+                <div class="overlay-header">
+                  <h3>
+                    Laser Field
+                  </h3>
+                </div>
+              </a>
+            </div>
+            <img src="/resources/Images/laserfield1.png" class="animated expand-image rounded border-black">
+          </div>
+        </div>
+
       </div>
     </div>
 
@@ -63,20 +79,26 @@
 
     <div class="jumbotron section-darkgreen">
       <div class="container text-spaced">
-        <div id="content-laserfield">
+
+        <div id="content-cprvr">
           <div class="col-sm-6">
             <h2 class="bold">
-              iOS App - Laser Field
+              Unity App - CPR-VR
             </h2>
             <h3>
-              "Dodge lasers in this new and amazing bounding game!" <br>
+              A virtual reality simulator for basic CPR training! <br>
+              "Our app gamifies a basic CPR procedure, making it easily accessible with Google Cardboard!" <br>
+              Programmed in C# with Unity.
+              Used Google Cardboard, Leap Motion, and SketchUp.
+              Made with Ming (Michelle) Yi, Aanand Bajaj, and Samuel Sun.
             </h3>
+            <br>
             <p>
-              Unfortunately, you can't find this on the App Store anymore.
+              Learn more about this project <a href="https://www.hackerearth.com/fr/sprints/uofthacks-iv/dashboard/UofTTeam1/submission/">here</a>.<br>
             </p>
           </div>
           <div class="col-sm-6">
-            <img src="resources/Images/laserfield2.png">
+            <img src="resources/Images/CPRVR2.jpg">
           </div>
         </div>
 
@@ -86,16 +108,43 @@
               iOS App - Stux
             </h2>
             <h3>
+              An engaging and minimalistic stacking game! <br>
               "Balance blocks in this new and incredible arcade stacker!" <br>
+              Programmed in Objective-C. <br>
+              Made by myself. <br>
             </h3>
+            <br>
             <p>
-              Unfortunately, you can't find this on the App Store anymore.
+              (Unfortunately, you can't find this on the App Store anymore.)
             </p>
           </div>
           <div class="col-sm-6">
             <img src="resources/Images/stux2.png">
           </div>
         </div>
+
+        <div id="content-laserfield">
+          <div class="col-sm-6">
+            <h2 class="bold">
+              iOS App - Laser Field
+            </h2>
+            <h3>
+              A simple bounding game - you control your character with your finger! <br>
+              "Dodge lasers in this new and amazing bounding game!" <br>
+              Programmed in Objective-C <br>
+              Made by myself. <br>
+            </h3>
+            <br>
+            <p>
+              (Unfortunately, you can't find this on the App Store anymore.)
+            </p>
+          </div>
+          <div class="col-sm-6">
+            <img src="resources/Images/laserfield2.png">
+          </div>
+        </div>
+
+
       </div>
     </div>
 
