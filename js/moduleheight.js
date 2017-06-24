c0ba8473700803d86d135050a29c05b0f5010e68
William Qin <william.qin51@gmail.com>
2017-06-23 20:42:14 -0400

    tysast

--- js/moduleheight.js
+++ js/moduleheight.js
@@ -1,7 +1,28 @@
 $(function() {
   var div = $('.module.module-1by1');
-  div.css('height', div.width());
+  div.css('height', div.width()*(1/1));
 
   var div = $('.module.module-2by1');
-  div.css('height', div.width());
+  div.css('height', div.width()*(1/2));
+
+  var div = $('.module.module-1by2');
+  div.css('height', div.width()*(2/1));
+
+  var div = $('.module.module-4by3');
+  div.css('height', div.width()*(3/4));
+
+  var div = $('.module.module-3by4');
+  div.css('height', div.width()*(4/3));
+
+  var div = $('.module.module-3by5');
+  div.css('height', div.width()*(5/3));
+
+  var div = $('.module.module-5by3');
+  div.css('height', div.width()*(3/5));
+
+  var div = $('.module.module-9by16');
+  div.css('height', div.width()*(16/9));
+
+  var div = $('.module.module-16by9');
+  div.css('height', div.width()*(9/16));
 })
--- portfolio.php
+++ portfolio.php
@@ -27,15 +27,45 @@
         <div class="col-sm-4 module-container">
           <div class="module module-2by1">
             <div class="overlay">
-              <a href="/portfolio.php" class="animated expand-container hidden-container button transparent-alt">
+              <a href="/home.php" class="animated expand-container hidden-container button transparent-alt">
                 <div class="overlay-header">
                   <h3>
-                    Portfolio
+                    Look!
                   </h3>
                 </div>
               </a>
             </div>
             <img src="/resources/Splash/about-4by2.jpg" class="animated expand-image rounded border-black">
+          </div>
+        </div>
+
+        <div class="col-sm-4 module-container">
+          <div class="module module-1by1">
+            <div class="overlay">
+              <a href="/home.php" class="animated expand-container hidden-container button transparent-alt">
+                <div class="overlay-header">
+                  <h3>
+                    Look!
+                  </h3>
+                </div>
+              </a>
+            </div>
+            <img src="/resources/Splash/contact-1by1.jpg" class="animated expand-image rounded border-black">
+          </div>
+        </div>
+
+        <div class="col-sm-4 module-container">
+          <div class="module module-3by5">
+            <div class="overlay">
+              <a href="/home.php" class="animated expand-container hidden-container button transparent-alt">
+                <div class="overlay-header">
+                  <h3>
+                    Look!
+                  </h3>
+                </div>
+              </a>
+            </div>
+            <img src="/resources/Splash/portfolio-3by6.jpeg" class="animated expand-image rounded border-black">
           </div>
         </div>
 
