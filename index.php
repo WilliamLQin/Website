e0ee697c1820f9a352817da705a29b8aeb8c6991
William Qin <william.qin51@gmail.com>
2017-08-20 01:00:55 -0400

    Index: resized content

--- index.php
+++ index.php
@@ -9,11 +9,10 @@
 
     <div class="jumbotron vertical-center section-darkblue">
       <div class="container">
-        <div class="col-sm-2"></div>
-        <div class="col-sm-3">
+        <div class="col-xs-5">
           <img src="/resources/Images/william4-square.jpg" class="img-circle">
         </div>
-        <div class="col-sm-5">
+        <div class="col-xs-7">
           <h1>
             <span class="light">WILLIAM</span> <span class="bold">QIN</span>
           </h1>
@@ -22,7 +21,6 @@
             Student & Developer
           </h2>
         </div>
-        <div class="col-sm-2"></div>
       </div>
     </div>
 
