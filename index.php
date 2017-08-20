857e0e322528a9f25be95f1a2ca8ff9ee396f584
William Qin <william.qin51@gmail.com>
2017-08-20 01:06:11 -0400

    Index: css positioning

--- index.php
+++ index.php
@@ -7,12 +7,12 @@
 
   <div>
 
-    <div class="jumbotron vertical-center section-darkblue">
+    <div class="jumbotron vertical-center vertical-fill section-darkblue">
       <div class="container">
-        <div class="col-xs-5">
-          <img src="/resources/Images/william4-square.jpg" class="img-circle">
+        <div class="col-xs-5 text-center">
+          <img src="/resources/Images/william4-square.jpg" class="img-circle" style="max-width: 360px">
         </div>
-        <div class="col-xs-7">
+        <div class="col-xs-7 vertical-center">
           <h1>
             <span class="light">WILLIAM</span> <span class="bold">QIN</span>
           </h1>
