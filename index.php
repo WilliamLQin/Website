4a8a7992bc18c5c4ea8a9dd51d7cb413216f1772
William Qin <william.qin51@gmail.com>
2017-08-20 01:00:01 -0400

    Index: wrapped content in container

--- index.php
+++ index.php
@@ -8,20 +8,22 @@
   <div>
 
     <div class="jumbotron vertical-center section-darkblue">
-      <div class="col-sm-2"></div>
-      <div class="col-sm-3">
-        <img src="/resources/Images/william4-square.jpg" class="img-circle">
+      <div class="container">
+        <div class="col-sm-2"></div>
+        <div class="col-sm-3">
+          <img src="/resources/Images/william4-square.jpg" class="img-circle">
+        </div>
+        <div class="col-sm-5">
+          <h1>
+            <span class="light">WILLIAM</span> <span class="bold">QIN</span>
+          </h1>
+          <hr>
+          <h2>
+            Student & Developer
+          </h2>
+        </div>
+        <div class="col-sm-2"></div>
       </div>
-      <div class="col-sm-5">
-        <h1>
-          <span class="light">WILLIAM</span> <span class="bold">QIN</span>
-        </h1>
-        <hr>
-        <h2>
-          Student & Developer
-        </h2>
-      </div>
-      <div class="col-sm-2"></div>
     </div>
 
   </div>
