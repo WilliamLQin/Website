bbc40f960a1a59448125108c59928d58ad5037ff
William Qin <william.qin51@gmail.com>
2017-08-20 02:15:15 -0400

    Add Font Awesome

--- php/imports.php
+++ php/imports.php
@@ -8,14 +8,17 @@
 
 <link rel="stylesheet" href="/css/style.css" type="text/css">
 
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
 <!-- jQuery library -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 
-<!-- Latest compiled JavaScript -->
+<!-- Bootstrap: Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+<!-- Bootstrap: Latest compiled JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<!-- Font Awesome -->
+<script src="https://use.fontawesome.com/55ea5c8061.js"></script>
 
 <!-- Google Analytics -->
 <script>
